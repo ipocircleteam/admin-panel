00ec44e89f03a1447344140bc651dd34e80d428b
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-08-26 09:38:53 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './globals.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-primary text-center text-[2rem] font-bold m-2'>Dashboard</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+
+    screens: {
+      sm: '480px',
+      md: '600px',
+      mdlg: '800px',
+      lg: '976px',
+      lgx: '1200px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      base: ['Raleway', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        base: '#FFFFFF',
+        baseTwo: '#D9D9D9',
+        primary: '#012C7A',
+        secondary: '#00BE9D',
+        primaryShade: '#C0DDF9',
+        panelDark: '#213555',
+        panelLight: '#19376D',
+      },
+    }
+  },
+  plugins: [],
+}
+
